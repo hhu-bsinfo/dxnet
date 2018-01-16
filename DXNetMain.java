c122700cf0ac5104fc75255dc4fbc30d4ada1759
Kevin Beineke <kevin.beineke@hhu.de>
2018-01-16 13:47:49 +0100

    - small update

--- DXNetMain.java
+++ DXNetMain.java
@@ -115,7 +115,7 @@ public final class DXNetMain implements MessageReceiver {
         setupDXNet();
 
         try {
-            Thread.sleep(30000);
+            Thread.sleep((ms_targetNodeIds.size() - ms_ownNodeId) * 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
