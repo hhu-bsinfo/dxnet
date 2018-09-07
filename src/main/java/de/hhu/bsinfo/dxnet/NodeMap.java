e8257a1f625054b9e0c29d0067583b5420cb7772
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2018-09-07 15:07:36 +0200

    NodeMap: Extend to support dynamic node joining and updating

--- src/main/java/de/hhu/bsinfo/dxnet/NodeMap.java
+++ src/main/java/de/hhu/bsinfo/dxnet/NodeMap.java
@@ -17,6 +17,7 @@
 package de.hhu.bsinfo.dxnet;
 
 import java.net.InetSocketAddress;
+import java.util.List;
 
 /**
  * An interface to map NodeIDs
@@ -24,6 +25,28 @@ import java.net.InetSocketAddress;
  * @author Kevin Beineke, kevin.beineke@hhu.de, 20.11.2015
  */
 public interface NodeMap {
+    /**
+     * Listener for node map events
+     */
+    public interface Listener {
+        /**
+         * A node mapping was added
+         *
+         * @param p_nodeId
+         *         Node id of added mapping
+         * @param p_address
+         *         Address of node
+         */
+        void nodeMappingAdded(final short p_nodeId, final InetSocketAddress p_address);
+
+        /**
+         * A node mapping was removed
+         *
+         * @param p_nodeId
+         *         Node id of removed mapping
+         */
+        void nodeMappingRemoved(final short p_nodeId);
+    }
 
     /**
      * Returns the NodeID
@@ -40,4 +63,58 @@ public interface NodeMap {
      * @return the address
      */
     InetSocketAddress getAddress(final short p_nodeID);
+
+    /**
+     * Get all currently available mappings
+     *
+     * @return List of mappings
+     */
+    List<Mapping> getAvailableMappings();
+
+    /**
+     * Register a listener for node mapping events
+     *
+     * @param p_listener
+     *         Listener to register
+     */
+    void registerListener(final Listener p_listener);
+
+    /**
+     * Node mapping to socket address
+     */
+    public static class Mapping {
+        private final short m_nodeId;
+        private final InetSocketAddress m_address;
+
+        /**
+         * Constructor
+         *
+         * @param p_nodeId
+         *         Node id
+         * @param p_address
+         *         Socket address
+         */
+        public Mapping(final short p_nodeId, final InetSocketAddress p_address) {
+            m_nodeId = p_nodeId;
+            m_address = p_address;
+        }
+
+        /**
+         * Get the node id
+         *
+         * @return Node id
+         */
+        public short getNodeId() {
+            return m_nodeId;
+        }
+
+        /**
+         * Get the socket address
+         *
+         * @return Socket address
+         */
+        public InetSocketAddress getAddress() {
+            return m_address;
+        }
+    }
 }
